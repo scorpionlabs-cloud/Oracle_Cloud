0ef3016346e39e3b7a8a161a379befc646ba5271
scorpionlabs-cloud <scorpion@scorpionlabs.org>
2025-09-13 15:27:41 +0800

    Update provider.tf

--- Terraform/oci_vm/provider.tf
+++ Terraform/oci_vm/provider.tf
@@ -1,7 +1,15 @@
-provider “oci” {
-	tenancy_ocid. = var.tenancy_ocid
-	user_ocid. = var.user_ocid
-	fingerprint. = var.fingerprint
-	private_key_path = var.private_key_path
-	region. = var.region
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 6.0.0"
+    }
+  }
 }
+
+provider "oci" {
+  region              = var.region
+  config_file_profile = var.config_file_profile
+}
+
